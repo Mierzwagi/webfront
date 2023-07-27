6000f19d3f43b807db8fffce6fffdd1e04fd3b24
Mierzwagi <giihmierzwa111@outlok.com>
2023-07-26 21:57:35 -0300

    Criar HTML, CSS e Script

---
+++ script.js
@@ -0,0 +1,21 @@
+/* ===== Introdução JavaScript */
+
+var nome = "Giovanna"
+let idade = 45
+const anoNascimento = 2003
+let idadeEsposa = "45"
+
+/* 
+= atribuição
+== comparar se é igual
+=== comparar se é exatamente igual
+ */
+
+
+console.log(idade === idadeEsposa);
+
+let listaComprsa = []
+
+listaComprsa.push(Maça)
+
+console.log(listaComprsa)
