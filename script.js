3d68d4ba4e8bc0f56de66e974988822b09ff4596
Mierzwagi <giihmierzwa111@outlok.com>
2023-07-27 21:49:20 -0300

    Criar input e salvar as informações

--- script.js
+++ script.js
@@ -1,17 +1,40 @@
+const titulo = document.getElementById("titulo")
+//Inputs
+const nomeInput = document.getElementById("nome")
+const emailInput = document.getElementById("e-mail")
+const telefoneInput = document.getElementById("telefone")
+//Outputs
+const resultado = document.getElementById("resultado")
+const resultado1 = document.getElementById("resultado1")
+const resultado2 = document.getElementById("resultado2")
+
+console.log(titulo.textContent)
+
+titulo.textContent = "Senai Cotia 2023"
+
+function  salvar(){
+
+    resultado.textContent = nomeInput.value
+    resultado1.textContent = emailInput.value
+    resultado2.textContent = telefoneInput.value
+
+}
+
+
 /* ===== Introdução JavaScript */
 
-var nome = "Giovanna"
+/* var nome = "Giovanna"
 let idade = 45
 const anoNascimento = 2003
 let idadeEsposa = "45"
-
+ */
 /* 
 = atribuição
 == comparar se é igual
 === comparar se é exatamente igual
  */
 
-var isOld = true;
+/* ar isOld = true;
 
 let num = 98 
 const pi = 3.14
@@ -22,16 +45,18 @@ if (idade == idadeEsposa) {
     console.log("igual")
 }else{
     console.log("diferente")
-}
+} */
 
 
 /* ----------------------- Array ---------------------------- */
 /* Acessmos o array fazendo reff ao index */
 
-let lista = [1,2,3,4,5,6]
+/* let lista = [1,2,3,4,5,6]
 for (let index = 0; index < lista.length; index++) {
     const element = lista[index];
     
     console.log(element)
-}
+} */
+
+
 
